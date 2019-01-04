f482d01cfe36bda6ca64ec8fb70aa5618134ae54
jmjurado23 <jmjurado23@gmail.com>
2019-01-04 17:09:40 +0100

    First commit

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+
+// Guarda en el cache dinámico
+function actualizaCacheDinamico( dynamicCache, req, res) {
+  if ( res.ok ) {
+    caches.open(dynamicCache).then(cache => {
+      cache.put(req, res.clone() );
+      return res.clone();
+    });
+  } else {
+    return res;
+  }
+}
